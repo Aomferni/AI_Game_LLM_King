2d5cd5faf192e5f662e5d9a360f1b12491df801c
Amy <zlx115hao@qq.com>
2023-12-07 00:37:02 +0800

    为啥不继续做判断了

--- Jin_Game/first_character_dance/final/qianfan.js
+++ Jin_Game/first_character_dance/final/qianfan.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 var dialogue1 = "在一个不情不愿的婚礼现场，作为新娘，心中装着另一个人，但必须虚与委蛇，她内心自说自话着";
 var prompt_assessment = "**你是一个友好度给分系统**，会根据用户话语的友好度给出 -2~2 分，**仅返回分数，不要评论！！！不要文字！！！**；比如 '你混蛋' = -2";
-var prompt_dialogue_generation = "**你是一个台词生成器**，会根据给定的上下文生成台词，**只给出台词**，**不超过20个字**";
+var prompt_dialogue_generation = "**你是一个台词生成器**，会根据给定的上下文生成台词，**只给出台词，不需要句号和双引号**，**不超过20个字，只给出一个版本**";
 
 async function countValue(message) {
     var options = {
@@ -35,7 +35,7 @@ async function countValue(message) {
     }
 }
 
-async function dialogueGenerate(context) {
+async function dialogueGenerate(context, callback) {
     var options = {
         'method': 'POST',
         'url': config.ER4_0_URL,
@@ -73,10 +73,10 @@ async function dialogueGenerate(context) {
     });
 }
 
-// module.exports = {
-//     countValue,
-//     dialogueGenerate
-// };
+module.exports = {
+    countValue,
+    dialogueGenerate
+};
 // console.log(countValue("你真没用"));
 // // countValue("我好无聊");
 // dialogueGenerate(dialogue1);
--- Jin_Game/first_character_dance/final/server.js
+++ Jin_Game/first_character_dance/final/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+// const { fetch } = await import('node-fetch');
 const cheerio = require('cheerio');
 const qianfan = require('./qianfan'); 
+async function init() {
+    const { fetch } = await import('node-fetch');
+    // 你的代码逻辑
+}
+
+init();
 
 const app = express();
 const port = 3000; // 您可以选择任何您喜欢的端口号
@@ -20,12 +27,10 @@ app.get('/', (req, res) => {
 
 app.get('/updateContent', async (req, res) => {
     try {
-      // 从请求中获取输入值
-    //   const context = req.query.context;
-      const context = "在一个不情不愿的婚礼现场，作为新娘，心中装着另一个人，但必须虚与委蛇，她内心自说自话着";
-  
-      // 调用 dialogueGenerate 函数
-      qianfan.dialogueGenerate(context, function(result) {
+        const context = req.query.context;  // 从请求中获取参数
+    // const context = "在一个不情不愿的婚礼现场，作为新娘，心中装着另一个人，但必须虚与委蛇，她内心自说自话着";
+        // 调用 dialogueGenerate 函数
+        qianfan.dialogueGenerate(context, function(error, result) {
         // 将结果更新到名为"大婚台词"的元素
         res.send(result);
       });
